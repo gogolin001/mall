f5e137cb244ac878dfb4990c53a14d212b0a8eaf
邓盛林 <330468156@qq.com>
2022-10-09 18:16:49 +0800

    修复运行

--- mall-common/src/main/java/com/lam/mall/common/config/BaseRedisConfig.java
+++ mall-common/src/main/java/com/lam/mall/common/config/BaseRedisConfig.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator
 import com.lam.mall.common.service.RedisService;
 import com.lam.mall.common.service.impl.RedisServiceImpl;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter;
