ca5afaf28e3cbc57d8bfd4a7ff23a67b19ebc04b
邓盛林 <330468156@qq.com>
2022-10-28 14:06:31 +0800

    二级缓存

--- mall-admin/src/main/java/com/lam/mall/admin/service/SysAuthorityService.java
+++ mall-admin/src/main/java/com/lam/mall/admin/service/SysAuthorityService.java
@@ -1,12 +1,18 @@
 package com.lam.mall.admin.service;
 
+import com.alicp.jetcache.Cache;
+import com.alicp.jetcache.CacheManager;
 import com.alicp.jetcache.anno.CacheType;
 import com.alicp.jetcache.anno.Cached;
+import com.alicp.jetcache.anno.CreateCache;
+import com.alicp.jetcache.template.QuickConfig;
 import com.lam.mall.mbg.maper.SysAuthorityMapper;
 import com.lam.mall.mbg.model.SysAuthority;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.time.Duration;
 import java.util.List;
 
 @Service
@@ -15,9 +21,26 @@ public class SysAuthorityService {
     @Autowired
     private SysAuthorityMapper authorityMapper;
 
+    @Autowired
+    private CacheManager cacheManager;
+    private Cache<Long, SysAuthority> authorityCache;
 
-    @Cached(name="authority-", expire = 3600, cacheType = CacheType.BOTH)
+    @PostConstruct
+    public void init() {
+        QuickConfig qc = QuickConfig.newBuilder("authority")
+                //.expire(Duration.ofSeconds(3600))
+                .cacheType(CacheType.BOTH) // two level cache
+                .localLimit(0)
+                .syncLocal(true) // invalidate local cache in all jvm process after update
+                .build();
+        authorityCache = cacheManager.getOrCreateCache(qc);
+        authorityCache.config().setLoader(key -> authorityMapper.selectById(key));
+    }
+
+
     public List<SysAuthority> listAll(){
+
+        var data = authorityCache.getAll(null);
         return authorityMapper.list();
     }
 }
